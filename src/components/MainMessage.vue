4c2b206136d06df80e6539e78ac31f4fb7058387
mdturp <m.dennisturp@gmail.com>
2023-03-13 23:17:48 +0100

    Update Scrollytelling

---
+++ src/components/MainMessage.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="pb-[70vh]">
+    <p class="max-w-[400px] bg-gray-200 px-[16px] py-[10px]">
+      In St. Moritz wurde im Sommer 1879 die erste Gleichstrom-Lichtbogen-Beleuchtungsanlage der
+      Schweiz und das erste kleine Wasserkraftwerk mit 7 kW zur Beleuchtung des Speisesaals im Kulm
+      Hotel St. Moritz des Hoteliers Johannes Badrutt in Betrieb genommen.
+    </p>
+  </div>
+</template>
--- src/components/TheStory.vue
+++ src/components/TheStory.vue
@@ -1,11 +1,49 @@
 <script setup>
 import MainHeader from '@/components/MainHeader.vue'
 import MainTimeline from '@/components/MainTimeline.vue'
+import MainMessage from '@/components/MainMessage.vue'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { onMounted, ref } from 'vue'
+
+// const timeLineFixed = ref(false)
+
+// onMounted(() => {
+//   gsap.registerPlugin(ScrollTrigger)
+
+//   ScrollTrigger.create({
+//     trigger: '#MyContainer',
+//     start: 'top 200px',
+//     end: 'bottom 250px',
+//     endTrigger: '#timeLine',
+//     markers: { startColor: 'black', endColor: 'black' },
+//     scrub: true,
+//     onEnter: () => logRed('onEnter'),
+//     onLeave: () => fixLine('onLeave'),
+//     onEnterBack: () => fixLine('onLeave'),
+//     onLeaveBack: () => logRed('onLeaveBack'),
+//     onRefresh: () => logRed('onRefresh')
+//     //   onUpdate: (self) => rollCircleRoll(self.progress.toFixed(2))
+//   })
+// })
+
+function logRed(m) {
+  console.log(m)
+}
+
+function fixLine(m) {
+    console.log(m)
+    timeLineFixed.value = !timeLineFixed.value
+
+}
 </script>
 
 <template>
-  <div id="parentContainer" class="font-serif mx-auto max-w-2xl">
+  <div id="parentContainer" class="relative font-serif mx-auto max-w-2xl">
     <MainHeader />
-    <MainTimeline />
+    <MainTimeline/>
+    <MainMessage />
+    <MainMessage />
   </div>
 </template>
